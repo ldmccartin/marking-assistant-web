eeb6aa44706b2c61d3b881b5bd4ae4b0b040db73
Luke McCartin <ldmccartin@gmail.com>
2022-12-12 22:10:04 +0100

    why?!

--- webpack.config.ts
+++ webpack.config.ts
@@ -36,8 +36,8 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[fullhash].bundle.js',
-    chunkFilename: 'vendor-[fullhash].bundle.js',
+    filename: 'static/js/[name]-[fullhash].bundle.js',
+    chunkFilename: 'static/js/vendor-[fullhash].bundle.js',
     publicPath: "/",
   },
 };
